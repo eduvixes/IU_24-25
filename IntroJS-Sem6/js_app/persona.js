dc60a0f0c4a0165fef8fa1af51cd5bcf13f9f58f
eduvixes <jrodeiro@gmail.com>
2024-10-25 17:14:03 +0200

    version para introducir eleccion de columnas a mostrar dinamica refactorizado en clase entidad. pendiente elevar a clase abstracta

--- IntroJS-Sem6/js_app/persona.js
+++ IntroJS-Sem6/js_app/persona.js
@@ -620,4 +620,133 @@ class persona extends EntidadAbstracta{
 	        }
 	}
 
+	crearTablaDatos(){
+
+		document.getElementById("id_tabla_datos").style.display = 'block';
+		//this.mostrarTitulos(this.columnasamostrar);
+		//this.mostrarDatos(this.entidad, this.datos, this.columnasamostrar);
+
+		//construir tabla
+		this.hacertabla();
+		//construir select
+		this.construirSelect();
+
+		//ocultar segun columnasamostrar
+		this.mostrarocultarcolumnas();
+	}
+
+	modificarcolumnasamostrar(atributo){
+
+		console.log(this.columnasamostrar);
+		let nuevascolumnas = Array();
+		if (this.columnasamostrar.includes(atributo)){
+			// borrar ese atributo
+			for (let i=0;i<this.columnasamostrar.length;i++){
+				if (this.columnasamostrar[i] == atributo){}
+				else{
+					nuevascolumnas.push(this.columnasamostrar[i]);
+				}
+			}
+			this.columnasamostrar = nuevascolumnas;
+		}
+		else{
+			// añadir
+			this.columnasamostrar.push(atributo);
+		}
+		console.log(this.columnasamostrar);
+
+		this.crearTablaDatos();
+	}
+
+	mostrarocultarcolumnas(){
+
+		for (let columna of this.atributos){
+			if (this.columnasamostrar.includes(columna)){}
+			else{
+				document.querySelector("th[class='"+columna+"']").style.display = 'none';
+				let arraytds = document.querySelectorAll("td[class='"+columna+"']");
+				for (let i=0;i<arraytds.length;i++){
+					arraytds[i].style.display = 'none';
+				}
+			}
+		}
+
+
+	}
+	construirSelect(){
+
+		document.getElementById("seleccioncolumnas").innerHTML = '';
+		for (let atributo of this.atributos){
+			let optionselect = document.createElement('option');
+			optionselect.class = atributo;
+			optionselect.value = atributo;
+			optionselect.innerHTML = atributo;
+			optionselect.setAttribute("onclick","validar.modificarcolumnasamostrar('"+atributo+"');");
+			if (this.columnasamostrar.includes(atributo)){
+				optionselect.selected = true;
+			}
+			document.getElementById("seleccioncolumnas").append(optionselect);
+		}
+	}
+
+	hacertabla(){
+
+		// titulos
+
+		this.atributos = Object.keys(this.datos[0]);
+
+		var textolineatitulos = '<tr>';
+
+		for (let atributo of this.atributos){
+		
+        	textolineatitulos += '<th class="'+atributo+'">'+atributo+'</th>';
+        
+		}  
+            
+		textolineatitulos += '<th colspan="3"></th>';
+        
+        textolineatitulos += '</tr>';
+        
+        let cabecera = document.getElementById("titulostablacabecera");
+        cabecera.innerHTML = textolineatitulos;
+
+		// filas
+
+		var textolineadatos = ''; 
+
+		for (let i=0;i<this.datos.length;i++){
+        
+			textolineadatos += '<tr style="background-color:grey;">';
+
+			for (let clave in this.datos[i]){
+
+				if (this.datosespecialestabla.includes(clave)){
+					let valorcolumna = this.cambiardatosespecialestabla(clave,this.datos[i][clave]);
+					textolineadatos += '<td class="'+clave+'">'+valorcolumna+'</td>';
+				}
+				else{
+					textolineadatos += '<td class="'+clave+'">'+this.datos[i][clave]+'</td>';
+				}
+	
+			}
+	
+			// crear los td para cada boton de llamada a funcion de formulario de accion (EDIT, DELETE O SHOWCURRENT)
+	
+			let lineaedit = this.crearboton(this.entidad, 'EDIT', JSON.stringify(this.datos[i]));
+			let lineadelete = this.crearboton(this.entidad, 'DELETE', JSON.stringify(this.datos[i]));
+			let lineashowcurrent = this.crearboton(this.entidad, 'SHOWCURRENT', JSON.stringify(this.datos[i]));
+	
+			textolineadatos += lineaedit+lineadelete+lineashowcurrent;
+	
+			textolineadatos += '</tr>';
+	
+		}
+		
+		let cuerpo = document.getElementById('muestradatostabla');
+		cuerpo.innerHTML = textolineadatos;
+
+	}
+
+
+
 }
